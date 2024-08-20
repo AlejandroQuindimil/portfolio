23b02dfac351c2417ecc72ab3858f824d87e6f85
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2024-08-20 12:07:10 +0200

    add header and nav components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from "./header/header.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent],
+  imports: [RouterOutlet, AboutComponent, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
