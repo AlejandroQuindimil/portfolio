f0e6c1a7f6105f7a1b3841e0bbe2beffeb8f896e
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2024-08-22 13:09:10 +0200

    add footer and links to social media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from "./header/header.component";
 import { NavComponent } from "./nav/nav.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, HeaderComponent, NavComponent],
+  imports: [RouterOutlet, AboutComponent, HeaderComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ToastrModule } from 'ngx-toastr';
+
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  correoElectronico = 'alejandro.quindimil.cancio@gmail.com';
+
+  // constructor(private toastr: ToastrService) {}
+
+  copiarCorreo() {
+    const correoElectronicoElement = document.getElementById('correo-electronico');
+    if (correoElectronicoElement) {
+      const correoElectronicoText = correoElectronicoElement.textContent;
+      if (correoElectronicoText) {
+        navigator.clipboard.writeText(correoElectronicoText);
+        // this.toastr.success('Correo electrónico copiado!');
+      }
+    }
+  }
+}
