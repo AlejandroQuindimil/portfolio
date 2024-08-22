e3baf495658670cddca83d4a091b5c57b419d5ab
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2024-08-22 13:24:47 +0200

    config navegator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from "./header/header.component";
 import { NavComponent } from "./nav/nav.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ProyectsComponent } from "./proyects/proyects.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ExperienceComponent } from "./experience/experience.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, HeaderComponent, NavComponent, FooterComponent],
+  imports: [RouterOutlet, AboutComponent, HeaderComponent, NavComponent, FooterComponent, ProyectsComponent, ContactComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
