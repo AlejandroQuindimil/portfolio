ccff39818e0c9f4b6c9799bf0e7f81231112e8e5
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2024-11-27 18:37:57 +0100

    add ngmodule

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  // ...
+  imports: [
+    CommonModule,
+    // otros imports...
+  ],
+  // ...
+})
+export class AppModule { } 
