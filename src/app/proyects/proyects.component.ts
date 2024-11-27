58e61a2f0b44bd6c4b90d123a3ec6e5ce75532e2
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2024-11-27 18:38:47 +0100

    fix and update proyects

--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-proyects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './proyects.component.html',
   styleUrl: './proyects.component.css'
 })
 export class ProyectsComponent {
-  imagePath = 'assets/images/descarga.jpg';
+  images = [
+    '/descarga.jpg',
+    '/descarga1.jpg'
+  ];
+  currentIndex = 0;
+
+  images2 = [
+    '/descarga.jpg',
+    '/descarga1.jpg'
+  ];
+  currentIndex2 = 0;
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.updateTransform('--current-index1', this.currentIndex);
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.updateTransform('--current-index1', this.currentIndex);
+  }
+
+  next2() {
+    this.currentIndex2 = (this.currentIndex2 + 1) % this.images2.length;
+    this.updateTransform('--current-index2', this.currentIndex2);
+  }
+
+  prev2() {
+    this.currentIndex2 = (this.currentIndex2 - 1 + this.images2.length) % this.images2.length;
+    this.updateTransform('--current-index2', this.currentIndex2);
+  }
+
+  private updateTransform(variable: string, index: number) {
+    document.documentElement.style.setProperty(variable, index.toString());
+  }
 }
