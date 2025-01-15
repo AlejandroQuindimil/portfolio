d63f13c192d8123f3674f93e400607602ff53100
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2025-01-15 20:00:46 +0100

    update textos e iconos con enlaces y maquetacion

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public next: string = '¿Qué sigue?';
+  public contactme: string = '¡Pongámonos en contacto!';
+  public contact_text: string = 'Estaré encantado de recibir tu contacto si quieres hablar sobre nuevas oportunidades. Escríbeme, y te responderé tan pronto como vea tu mensaje. ';
+  public contactme2: string = 'Ponte en contacto';
+  public dmlinkedin: string = 'DM on Linkedin';
+
+
 
 }
